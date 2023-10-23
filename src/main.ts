36b6a42d14ab23ce16d41dbbfca4abdfbe2b97a1
abdallah-hatem <ahkortam@gmail.com>
2023-10-23 16:00:04 +0300

    changes in cors

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://admin-panel-next-js-delta.vercel.app',
-    ],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-  });
+  app.enableCors();
+
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://admin-panel-next-js-delta.vercel.app',
+  //   ],
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  // });
 
   await app.listen(8000);
 }
