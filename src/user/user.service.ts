0609c848c46a6f80ee14f73e47bc6865ca1a9702
abdallah-hatem <ahkortam@gmail.com>
2023-10-25 19:48:41 +0300

    error handling changes

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -54,14 +54,8 @@ export class UserService {
         },
       });
 
-      if (!user) throw new HttpException('Error in database', 500);
-
       return { user };
     } catch (error) {
-      if (error) {
-        const { message, statusCode } = error;
-        throw new HttpException(message, statusCode);
-      }
       return error;
     }
   }
